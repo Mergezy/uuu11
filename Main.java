b763c0b28961bc033513fc7370acc58941ee077a
Mergezy <azgardex9998@gmail.com>
2023-01-29 15:18:36 +0300

    Cart

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         for(byte i = 0; i <= 10; i++){
             System.out.println('213');
+            System.out.println("skjf");
             for(byte g = 0; g <= 10; g++){
                 System.out.printf("%d * %d = %d",i,g,(i*g));
                 System.out.println("");
