bac026b296739c628d8911c10e56803f6ec0643d
Mergezy <azgardex9998@gmail.com>
2023-01-29 15:13:21 +0300

    NewFunctional

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         for(byte i = 0; i <= 10; i++){
+            System.out.println('213');
             for(byte g = 0; g <= 10; g++){
                 System.out.printf("%d * %d = %d",i,g,(i*g));
                 System.out.println("");
