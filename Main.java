6a9513ac208499742182a75e9e60aa124137856f
Mergezy <azgardex9998@gmail.com>
2023-01-29 15:08:11 +0300

    init

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for(byte i = 0; i <= 10; i++){
+            for(byte g = 0; g <= 10; g++){
+                System.out.printf("%d * %d = %d",i,g,(i*g));
+                System.out.println("");
+            }
+            System.out.println("=========");
+        }
+    }
+}
