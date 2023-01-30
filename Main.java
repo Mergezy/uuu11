8e08bd8080256fdec81b5bd17100f63138945ac8
M3rg <123715188+Mergezy@users.noreply.github.com>
2023-01-31 00:39:12 +0300

    12

--- Main.java
+++ Main.java
@@ -10,3 +10,17 @@ public class Main {
         }
     }
 }
+
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException, IOException {
+        String Opener = null;
+        System.out.print("что хотите открыть?\nВведите название процесса: ");
+        while (true){
+        Scanner input = new Scanner(System.in);
+        Opener = input.nextLine(); // считываение команды
+        Runtime.getRuntime().exec(Opener); // открытие
+        }
+    }
+}
