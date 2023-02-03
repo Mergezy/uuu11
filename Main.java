d4e87c29fe2f455968991de6d6b40b54bb8ccd65
M3rg <123715188+Mergezy@users.noreply.github.com>
2023-02-03 13:55:35 +0300

    upd

--- Main.java
+++ Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        while (true){
-            int a = 1;
-            for(int i = 0; i < a; i++){
-                System.out.println(i);
-                a++;
-            }
+        short a = 128;
+        for(byte i = 0; i < a; i++){
+            System.out.println(i);
+
         }
     }
 }
+
+
+//public class Main {
+//    public static void main(String[] args) {
+//        while (true){
+//            byte a = 1;
+//            for(byte i = 0; i < a; i++){
+//                System.out.println(i);
+//                a++;
+//            }
+//        }
+//    }
+//}
